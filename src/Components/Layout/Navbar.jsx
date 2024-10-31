97ef24f70fd731beccaf27c992cde9b87cff519e
roaaabushaqrah <roaa.abushaqrah@gmail.com>
2024-10-31 20:43:14 +0300

    create components guide

--- src/Components/Layout/Navbar.jsx
+++ src/Components/Layout/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = ({ brand, links, bgColor = 'light' }) => {
     return (
-        <nav className={`navbar navbar-expand-lg navbar-${bgColor} bg-${bgColor}`}>
+        <nav className={`navbar navbar-expand-lg navbar-light bg-light sticky-top navbar-${bgColor} bg-${bgColor}`}>
             <div className="container">
                 <Link className="navbar-brand" to="/">
                     {brand}
